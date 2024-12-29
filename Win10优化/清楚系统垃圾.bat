8588843325420c6aef01006b7286ad067d9cb4cf
danmo <49153731+ScorpionFH@users.noreply.github.com>
2024-12-30 00:37:05 +0800

    Create 清楚系统垃圾.bat

---
+++ Win10优化/清楚系统垃圾.bat
@@ -0,0 +1,28 @@
+@echo off
+echo 开始清除系统垃圾
+pause
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp
+del /f /s /q %systemdrive%\*.log
+del /f /s /q %systemdrive%\*.gid
+del /f /s /q %systemdrive%\*.chk
+del /f /s /q %systemdrive%\*.old
+del /f /s /q %systemdrive%\*.err 
+del /f /s /q %systemdrive%\*.wbk
+del /f /s /q %systemdrive%\*.xlk
+del /f /s /q %systemdrive%\*.diz
+del /f /s /q %systemdrive%\*.dmp 
+del /f /s /q %systemdrive%\*.ftg
+del /f /s /q %systemdrive%\*.fts 
+del /f /s /q %windir%\*.bak
+del /f /s /q %systemdrive%\recycled\*.*
+del /f /s /q %windir%\prefetch\*.*
+rd /s /q %windir%\temp & md %windir%\temp
+del /f /q %userprofile%\cookies\*.*
+del /f /q %userprofile%\recent\*.*
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*"
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*"
+del /f /s /q "%userprofile%\recent\*.*"
+echo 系统垃圾清除完毕
+echo. & pause
+exit
